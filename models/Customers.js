a3c6d33a5974495d602e8098e0e9c12bbf0b23d3
Yash Gupta <76595447+conqryash007@users.noreply.github.com>
2023-04-20 22:12:36 +0530

    updates

--- models/Customers.js
+++ models/Customers.js
@@ -1,75 +1,75 @@
-const mongoose=require('mongoose')
+const mongoose = require("mongoose");
 
-const customerSchema= new mongoose.Schema({
-    name:{
-        type:String,
-        required:true
-    },
-    address:{
-        type:String
-    },
-    dob:{
-        type:Date,
-    },
-    partnerUid:{
-        type:String,
-        required:true
-    },
-    userUid:{
-        type:String,
-        required:true
-    },
-    childListUid:{
-        type:String,
-        required:true
-    },
-    gender:{
-        type:String,
-    },
-    bloodGroup:{
-        type:String,
-    },
-    dateRegistered:{
-        type:String
-    },
-    portalId:{
-        type:String
-    },
-    email:{
-        type:String
-    },
-    mobile:{
-        type:String
-    },
-    
-    emergencyContactName1:{
-        type:String
-    },
-    emergencyContactName2:{
-        type:String
-    },
-    emergencyContactMobile1:{
-        type:String
-    },
-    emergencyContactMobile2:{
-        type:String
-    }
-    // emergencyContact1:{
-    //     name:{
-    //         type:String
-    //     },
-    //     contact:{
-    //         type:String
-    //     }
-    // },
-    // emergencyContact2:{
-    //     name:{
-    //         type:String
-    //     },
-    //     contact:{
-    //         type:String
-    //     }
-    // },
-})
+const customerSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+  },
+  dob: {
+    type: Date,
+  },
+  partnerUid: {
+    type: String,
+    required: true,
+  },
+  userUid: {
+    type: String,
+    required: true,
+  },
+  childListUid: {
+    type: String,
+    required: true,
+  },
+  gender: {
+    type: String,
+  },
+  bloodGroup: {
+    type: String,
+  },
+  dateRegistered: {
+    type: String,
+  },
+  portalId: {
+    type: String,
+  },
+  email: {
+    type: String,
+  },
+  mobile: {
+    type: String,
+  },
 
-module.exports=mongoose.model('Customer',customerSchema)
+  emergencyContactName1: {
+    type: String,
+  },
+  emergencyContactName2: {
+    type: String,
+  },
+  emergencyContactMobile1: {
+    type: String,
+  },
+  emergencyContactMobile2: {
+    type: String,
+  },
+  // emergencyContact1:{
+  //     name:{
+  //         type:String
+  //     },
+  //     contact:{
+  //         type:String
+  //     }
+  // },
+  // emergencyContact2:{
+  //     name:{
+  //         type:String
+  //     },
+  //     contact:{
+  //         type:String
+  //     }
+  // },
+});
+
+module.exports = mongoose.model("Customer", customerSchema);
